a443e7b15acaae9ce15a929ba7ce737884c1ea13
Dinh Duy <0903682500a@gmail.com>
2023-12-13 10:03:22 +0700

    commit

--- layouts/header.php
+++ layouts/header.php
@@ -14,7 +14,7 @@ if (!isLogin()) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <link rel="stylesheet" href="<?= _WEB_HOST_ROOT ?>/public/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= _WEB_HOST_ROOT ?>/public/css/myStyle.css">
     <link rel="stylesheet" href="<?= _WEB_HOST_ROOT ?>/public/css/myStyle.css?v1=<? rand() ?>">
     <title>Task List</title>
 </head>
